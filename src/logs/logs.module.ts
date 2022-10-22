b5eb755cf419474de4e4caec84718ad56cbb7eb5
GPlay97 <GPlay97@users.noreply.github.com>
2022-10-22 17:00:00 +0200

    ✨ store last sync data

--- src/logs/logs.module.ts
+++ src/logs/logs.module.ts
@@ -5,13 +5,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AccountModule } from 'src/account/account.module';
 import { Log, LogSchema } from './schemas/log.schema';
 import { MetadataHandler } from './handler/metadata';
+import { LastSyncHandler } from './handler/last-sync';
+import { LastSync, LastSyncSchema } from './schemas/last-sync.schema';
 
 @Module({
   controllers: [LogsController],
-  providers: [LogsService, MetadataHandler],
+  providers: [LogsService, MetadataHandler, LastSyncHandler],
   imports: [
     AccountModule,
-    MongooseModule.forFeature([{ name: Log.name, schema: LogSchema }]),
+    MongooseModule.forFeature([
+      { name: Log.name, schema: LogSchema },
+      { name: LastSync.name, schema: LastSyncSchema },
+    ]),
   ],
 })
 export class LogsModule {}
