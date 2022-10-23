7965c614f4f51d33d39bbaa884de1e1492ff1650
GPlay97 <GPlay97@users.noreply.github.com>
2022-10-23 21:39:58 +0200

    ✨ Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,18 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('EVNotify API')
+    .setDescription('Documentation for EVNotify API')
+    .setVersion('3.0')
+    .addTag('Account')
+    .addTag('Settings')
+    .addTag('Logs & Sync')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.SERVER_PORT);
 }
