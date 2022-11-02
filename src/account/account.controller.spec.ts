49b26c9ea5b381669cf68fefeaef1aea1844895f
GPlay97 <GPlay97@users.noreply.github.com>
2022-11-03 00:11:53 +0100

    ✅ Available AKey handling

--- src/account/account.controller.spec.ts
+++ src/account/account.controller.spec.ts
@@ -1,12 +1,16 @@
+import { NotFoundException } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import mongoose from 'mongoose';
 import { AccountController } from './account.controller';
 import { AccountModule } from './account.module';
+import { AKEY_LENGTH } from './dto/account.dto';
+import { AvailableAKeyDto } from './dto/available-akey.dto';
 
 describe('AccountController', () => {
   let controller: AccountController;
+  let randomAkey;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,4 +31,26 @@ describe('AccountController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should generate a random available akey', async () => {
+    const response = await controller.availableAKey();
+
+    expect(response).toBeInstanceOf(AvailableAKeyDto);
+    expect(response.akey).toHaveLength(AKEY_LENGTH);
+    randomAkey = response.akey;
+  });
+
+  it('should not be the same on new call', async () => {
+    const response = await controller.availableAKey();
+
+    expect(response.akey).not.toEqual(randomAkey);
+  });
+
+  it('should not be already taken', async () => {
+    await expect(async () => {
+      await controller.findOne(randomAkey);
+    }).rejects.toThrow(NotFoundException);
+  });
+
+  test.todo('account info should be protected');
 });
