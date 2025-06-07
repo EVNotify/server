324e22fcd8af65b5e069ee0f6cac843d932c01b2
GPlay97 <GPlay97@users.noreply.github.com>
2025-06-07 11:33:25 +0200

    🐛 metadata calculation

--- src/tripnotify/dto/trip-information.dto.ts
+++ src/tripnotify/dto/trip-information.dto.ts
@@ -1,6 +1,7 @@
 import { LogWithHistoryDto } from "./log-with-history.dto";
 import { Expose } from "class-transformer";
 import { TripMetadataDto } from "./trip-metadata.dto";
+import { TYPE } from "src/logs/entities/type.entity";
 
 export class TripInformationDto {
   constructor(logs: LogWithHistoryDto[]) {
@@ -15,22 +16,30 @@ export class TripInformationDto {
     let rechargedEnergy = 0;
     let driveSpeed = 0;
     let chargeSpeed = 0;
+    let countChargeLogs = 0;
+    let countDriveLogs = 0;
 
     this.logs.forEach((log) => {
-      consumedEnergy += log.dischargedKWh;
-      rechargedEnergy += log.rechargedKWh;
-      driveSpeed += log.averageSpeed;
-      chargeSpeed += log.averageKW;
+      consumedEnergy += log.dischargedKWh || 0;
+      rechargedEnergy += log.rechargedKWh || 0;
+
+      if (log.type === TYPE.CHARGE || (log.type === TYPE.UNKNOWN && log.averageKW)) {
+        chargeSpeed += Math.abs(log.averageKW || 0);
+        countChargeLogs++;
+      } else if (log.type === TYPE.DRIVE || (log.type === TYPE.UNKNOWN && log.averageSpeed)) {
+        driveSpeed += log.averageSpeed || 0;
+        countDriveLogs++;
+      }
     });
 
-    driveSpeed = (driveSpeed / this.logs.length) || 0;
-    chargeSpeed = (chargeSpeed / this.logs.length) || 0;
+    driveSpeed = countDriveLogs ? driveSpeed / countDriveLogs : 0;
+    chargeSpeed = countChargeLogs ? chargeSpeed / countChargeLogs : 0;
 
     return new TripMetadataDto(
-      consumedEnergy || 0,
-      rechargedEnergy || 0,
-      driveSpeed,
-      chargeSpeed,
+      parseFloat((consumedEnergy || 0).toFixed(2)),
+      parseFloat((rechargedEnergy || 0).toFixed(2)),
+      parseFloat((driveSpeed || 0).toFixed(2)),
+      parseFloat((chargeSpeed || 0).toFixed(2)),
     );
   }
 }
