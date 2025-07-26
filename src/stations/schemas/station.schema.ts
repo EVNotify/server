87bd9a80da6d531a2e144ab7796b1e809edef8a8
GPlay97 <GPlay97@users.noreply.github.com>
2025-07-26 12:03:36 +0200

    ✨ 2dsphere index and geojson point

--- src/stations/schemas/station.schema.ts
+++ src/stations/schemas/station.schema.ts
@@ -15,20 +15,42 @@ export class Station {
 
     if (partial.AddressInfo) {
       this.AddressInfo = new AddressInfo(partial.AddressInfo.Latitude, partial.AddressInfo.Longitude);
+      this.location = {
+        type: 'Point',
+        coordinates: [partial.AddressInfo.Longitude, partial.AddressInfo.Latitude],
+      };
     }
   }
 
   @Prop({ required: true, unique: true })
   ID: string;
 
-  @Prop({ required: true, unique: true})
+  @Prop({ required: true, unique: true })
   UUID: string;
 
   @Prop({ required: true, type: AddressInfo })
   AddressInfo: AddressInfo;
+
+  @Prop({
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point',
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  })
+  location: {
+    type: 'Point';
+    coordinates: [number, number];
+  };
 
   @Prop({ type: Object })
   data: any;
 }
 
 export const StationSchema = SchemaFactory.createForClass(Station);
+
+StationSchema.index({ location: '2dsphere' });
